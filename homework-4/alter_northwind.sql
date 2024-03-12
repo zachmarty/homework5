f6eede9c334848199c5c5bb75aa7a0f74f323f18
Pavel <pashapasha-2013@mail.ru>
2024-03-12 14:07:57 +0300

    homework 4 done

--- homework-4/alter_northwind.sql
+++ homework-4/alter_northwind.sql
@@ -1,12 +1,14 @@
 -- Подключиться к БД Northwind и сделать следующие изменения:
 -- 1. Добавить ограничение на поле unit_price таблицы products (цена должна быть больше 0)
-
+alter table products alter column unit_price int check (unit_price > 0);
 
 -- 2. Добавить ограничение, что поле discontinued таблицы products может содержать только значения 0 или 1
-
+alter table products alter column discontinued int check (discontinued = 1 or discontinued = 0);
 
 -- 3. Создать новую таблицу, содержащую все продукты, снятые с продажи (discontinued = 1)
-
+create table discontinued_products as select * from products where discontinued = 1;
 
 -- 4. Удалить из products товары, снятые с продажи (discontinued = 1)
 -- Для 4-го пункта может потребоваться удаление ограничения, связанного с foreign_key. Подумайте, как это можно решить, чтобы связь с таблицей order_details все же осталась.
+delete order_details from order_details join products on order_details.product_id = products.product_id where discontinued = 1;
+delete from products where discontinued = 1;
--- homework-4/alter_student.sql
+++ homework-4/alter_student.sql
@@ -1,19 +1,22 @@
 -- 1. Создать таблицу student с полями student_id serial, first_name varchar, last_name varchar, birthday date, phone varchar
-
+create table student (student_id serial, first_name varchar(20), last_name varchar(20), birthday date, phone varchar(15));
 
 -- 2. Добавить в таблицу student колонку middle_name varchar
-
+alter table student add column middle_name varchar(20)
 
 -- 3. Удалить колонку middle_name
-
+alter table student drop column middle_name;
 
 -- 4. Переименовать колонку birthday в birth_date
-
+alter table student rename column birthday to birth_date;
 
 -- 5. Изменить тип данных колонки phone на varchar(32)
-
+alter table student alter column phone varchar(32);
 
 -- 6. Вставить три любых записи с автогенерацией идентификатора
-
-
+insert into student (first_name, last_name, birth_date, phone) values ('vasya', 'pupkin', '2000-01-01', '88008880000');
+insert into student (first_name, last_name, birth_date, phone) values ('petya', 'petrov', '2000-02-02', '89008880000');
+insert into student (first_name, last_name, birth_date, phone) values ('sasha', 'sashov', '2000-03-03', '88008880000');
 -- 7. Удалить все данные из таблицы со сбросом идентификатор в исходное состояние
+truncate table student;
+alter sequence student_student_id_seq restart with 1;
